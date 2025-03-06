648747a1e732c8b7c30d8489c34b457835d0b050
Huimin22 <108915390+Huimin22@users.noreply.github.com>
2025-03-05 16:20:23 -0800

    Update upload_images.sh

--- upload_images.sh
+++ upload_images.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+set -e
 
-# Ensure INSTANCE_ID is passed from the environment
-if [ -z "$INSTANCE_ID" ]; then
-  echo "INSTANCE_ID is not set."
-  exit 1
-fi
+FRONTEND_IMAGE_ID=$(docker images --filter=reference="spapath-frontend" --format "{{.ID}}")
+BACKEND_IMAGE_ID=$(docker images --filter=reference="spapath-backend" --format "{{.ID}}")
 
-# Terminate the EC2 instance
-echo "Terminating EC2 instance with ID: $INSTANCE_ID"
-aws ec2 terminate-instances --instance-ids "$INSTANCE_ID"
+echo "Found Docker Image ID (frontend): $FRONTEND_IMAGE_ID"
+echo "Found Docker Image ID (backend): $BACKEND_IMAGE_ID"
+
+TIMESTAMP=$(date "+%Y%m%d.%H%M%S")
+echo "Generated TIMESTAMP: $TIMESTAMP"
+
+ECR_REPO_FRONTEND="738250824273.dkr.ecr.us-east-1.amazonaws.com/appfrontend"
+ECR_REPO_BACKEND="738250824273.dkr.ecr.us-east-1.amazonaws.com/appbackend"
+
+docker tag $FRONTEND_IMAGE_ID $ECR_REPO_FRONTEND:$TIMESTAMP
+docker tag $FRONTEND_IMAGE_ID $ECR_REPO_FRONTEND:latest
+docker push --all-tags $ECR_REPO_FRONTEND
+
+docker tag $BACKEND_IMAGE_ID $ECR_REPO_BACKEND:$TIMESTAMP
+docker tag $BACKEND_IMAGE_ID $ECR_REPO_BACKEND:latest
+docker push --all-tags $ECR_REPO_BACKEND
+
+echo "TIMESTAMP=$TIMESTAMP" >> $GITHUB_ENV
